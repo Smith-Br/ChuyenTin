92da9328c2af4a05ed29045eaeeeacf931e2dd5a
Smith-Br <Entired.0@Gmail.com>
2023-02-15 20:56:20 +0700

    New Update

--- CD2_BasicKnowledgement/2.2/22.cpp
+++ CD2_BasicKnowledgement/2.2/22.cpp
@@ -6,21 +6,23 @@ using namespace std;
 int main() {
 	fstream inp("22.inp"), out("22.out");
 	int n; inp >> n;
+	int tn = n;
 
 	int A[n];
 	for (int i = 0; i <= n; i++) A[i] = 0;
 
+
 	for (int i = 2; i <= sqrt(n); i++) {
 		while (n % i == 0) {
 			A[i]++;
-			A[n/i]++;
 			n = n / i;
 		}
 	}
-
-	for (int i = 2; i <= n/2; i++) {
+	
+	string str = "";
+	for (int i = 2; i <= sqrt(tn); i++) {
 		while (A[i] > 0) {
-			out << A[i] << '.';
+			out << i << endl;
 			A[i]--;
 		}
 	}
