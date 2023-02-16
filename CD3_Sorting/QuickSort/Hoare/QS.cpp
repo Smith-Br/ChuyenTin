b3b4db3f9ef3b766bbd2f2785afc7a38f14e01cd
Smith-Br <Entired.0@Gmail.com>
2023-02-16 21:38:37 +0700

    quickSort

--- CD3_Sorting/QuickSort/Hoare/QS.cpp
+++ CD3_Sorting/QuickSort/Hoare/QS.cpp
@@ -2,6 +2,39 @@
 
 using namespace std;
 
+int partition(int Arr[], int l, int r) {
+	int pivot = Arr[(l + r) / 2];
+	int i = l - 1, j = r + 1;
+	while (true) {
+		do {
+			i++;
+		} while (Arr[i] < pivot);
+		do {
+			j--;
+		} while (Arr[j] > pivot);
+		if (i < j) {
+			swap(Arr[i], Arr[j]);
+		} else return j;
+	}
+}
+
+void quickSort(int Arr[], int l, int r) {
+	if (l >= r) return;
+	int pivot = partition(Arr, l, r);
+	quickSort(Arr, l, pivot);
+	quickSort(Arr, pivot + 1, r);
+}
+
 int main() {
-	
+	fstream inp("QS.inp"), out("QS.out");
+	int n; inp >> n;
+	int Arr[100];
+	for (int i = 0; i < n; i++) {
+		inp >> Arr[i];
+	}
+	quickSort(Arr, 0, n - 1);
+	for (int i = 0; i < n; i++) {
+		out << Arr[i] << " ";
+	}
+	return 0;
 }
--- CD3_Sorting/QuickSort/Lomuto/QS.cpp
+++ CD3_Sorting/QuickSort/Lomuto/QS.cpp
@@ -2,12 +2,6 @@
 
 using namespace std;
 
-void swap(int a, int b) {
-	int temp = a;
-	a = b;
-	b = temp;
-}
-
 int partition(int Arr[], int l, int r) {
 	int pivot = Arr[r];
 	int i = l - 1;
@@ -17,10 +11,28 @@ int partition(int Arr[], int l, int r) {
 			swap(Arr[i], Arr[j]);
 		}
 	}
+	i++;
+	swap(Arr[i], Arr[r]);
+	return i;
+}
+
+void quickSort(int Arr[], int l, int r) {
+	if ( l >= r) return;
+	int pivot = partition(Arr, l, r);
+	quickSort(Arr, l, pivot - 1);
+	quickSort(Arr, pivot + 1, r);
 }
 
 int main() {
-	
-
+	fstream inp("QS.inp"), out("QS.out");
+	int n; inp >> n;
+	int Arr[100];
+	for (int i = 0; i < n; i++) {
+		inp >> Arr[i];
+	}
+	quickSort(Arr, 0, n - 1);
+	for (int i = 0; i < n; i++) {
+		out << Arr[i] << " ";
+	}
 	return 0;
 }
